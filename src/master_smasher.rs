2945c67769488c6cc1528b41f414d92d5b1d4aeb
Andres Medina <amedina@pivotal.io>
2016-10-31 11:48:59 -0700

    load meteor resource

--- src/master_smasher.rs
+++ src/master_smasher.rs
@@ -4,6 +4,7 @@ extern crate sdl2;
 
 use self::sdl2::event::Event;
 use self::sdl2::keyboard::Keycode;
+use self::sdl2::rect;
 
 use self::sdl2_image::{LoadTexture, INIT_PNG, INIT_JPG};
 use std::error::Error;
@@ -12,9 +13,14 @@ use std::path::Path;
 pub fn run() -> Result<(), Box<Error>> {
     let (mut renderer, mut event_pump) = try!(moho::init("Master Smasher", 800, 600));
     let _image_context = try!(sdl2_image::init(INIT_PNG | INIT_JPG));
-    let background_path = Path::new("resources/background_menu.png");
-    let texture = try!(renderer.load_texture(background_path));
-    try!(renderer.copy(&texture, None, None));
+    let background_path = Path::new("resources/background_game.png");
+    let meteor_path = Path::new("resources/meteor.png");
+    let background_texture = try!(renderer.load_texture(background_path));
+    let meteor_texture = try!(renderer.load_texture(meteor_path));
+    let query = meteor_texture.query();
+    let dst = rect::Rect::new(0, 0, query.width, query.height);
+    try!(renderer.copy(&background_texture, None, None));
+    try!(renderer.copy(&meteor_texture, None, Some(dst)));
     renderer.present();
 
     'running: loop {
