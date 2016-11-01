0326316d9b5c52dc4e67b71851edea670e07c7a7
Andres Medina <amedina@pivotal.io>
2016-11-01 11:07:59 -0700

    simplified input manager until it needs to do more; added test for releasing keys

--- src/moho/input_manager.rs
+++ src/moho/input_manager.rs
@@ -2,29 +2,16 @@ extern crate glm;
 
 use sdl2::keyboard::Keycode;
 use std::collections::HashSet;
-use self::glm::*;
 
 pub struct InputManagerImp {
     pressed_keys: HashSet<Keycode>,
-    previously_pressed_keys: HashSet<Keycode>,
-    mouse_coords: glm::Vector2<f64>,
 }
 
 impl InputManagerImp {
     pub fn new() -> Self {
         InputManagerImp {
             pressed_keys: HashSet::new(),
-            previously_pressed_keys: HashSet::new(),
-            mouse_coords: dvec2(0., 0.),
         }
-    }
-
-    pub fn update(&mut self) {
-        self.previously_pressed_keys = self.pressed_keys.clone();
-    }
-
-    pub fn set_mouse_coords(&mut self, x: f64, y: f64) {
-        self.mouse_coords = dvec2(x, y);
     }
 
     pub fn press_key(&mut self, keycode: Keycode) {
@@ -38,18 +25,6 @@ impl InputManagerImp {
     pub fn is_key_down(&self, keycode: Keycode) -> bool {
         self.pressed_keys.contains(&keycode)
     }
-
-    pub fn was_key_pressed(&self, keycode: Keycode) -> bool {
-        self.pressed_keys.contains(&keycode) && !self.previously_pressed_keys.contains(&keycode)
-    }
-
-    pub fn was_key_released(&self, keycode: Keycode) -> bool {
-        !self.pressed_keys.contains(&keycode) && self.previously_pressed_keys.contains(&keycode)
-    }
-
-    pub fn get_mouse_coords(&self) -> glm::Vector2<f64> {
-        self.mouse_coords
-    }
 }
 
 #[cfg(test)]
@@ -60,7 +35,18 @@ mod tests {
     #[test]
     fn it_adds_pressed_keys() {
         let mut subject = InputManagerImp::new();
+        assert_eq!(subject.is_key_down(Keycode::Down), false);
         subject.press_key(Keycode::Down);
         assert_eq!(subject.is_key_down(Keycode::Down), true);
+    }
+
+    #[test]
+    fn it_releases_keys() {
+        let mut subject = InputManagerImp::new();
+        subject.press_key(Keycode::Down);
+        subject.press_key(Keycode::Up);
+        subject.release_key(Keycode::Down);
+        assert_eq!(subject.is_key_down(Keycode::Down), false);
+        assert_eq!(subject.is_key_down(Keycode::Up), true);
     }
 }
