a64b807a9f9ce8d054acda7a0dd985a96a274174
Andres Medina <amedina@pivotal.io>
2016-11-04 16:36:59 -0700

    rectangle and circle intersect if the center of either is inside the other

--- src/shape.rs
+++ src/shape.rs
@@ -77,7 +77,13 @@ impl Shape for Circle {
     }
 
     fn intersects_with_rectangle(&self, rectangle: &Rectangle) -> bool {
-        false
+        if self.contains(&rectangle.center) {
+            true
+        } else if rectangle.contains(&self.center) {
+            true
+        } else {
+            false
+        }
     }
 
     fn intersects_with_circle(&self, circle: &Circle) -> bool {
@@ -191,5 +197,53 @@ mod test {
         };
         assert!(rectangle_a.intersects_with_rectangle(&rectangle_b));
         assert!(rectangle_b.intersects_with_rectangle(&rectangle_a));
+    }
+
+    #[test]
+    fn rectangle_circle_no_intersect() {
+        let rectangle = Rectangle {
+            dims: glm::dvec2(5 as f64, 2 as f64),
+            center: glm::dvec2(6 as f64, 0 as f64),
+        };
+
+        let circle = Circle {
+            radius: 3 as f64,
+            center: glm::dvec2(2 as f64, 3 as f64),
+        };
+
+        assert!(!rectangle.intersects_with_circle(&circle));
+        assert!(!circle.intersects_with_rectangle(&rectangle));
+    }
+
+    #[test]
+    fn rectangle_inside_circle() {
+        let rectangle = Rectangle {
+            dims: glm::dvec2(1 as f64, 2 as f64),
+            center: glm::dvec2(3 as f64, 5 as f64),
+        };
+
+        let circle = Circle {
+            radius: 5 as f64,
+            center: glm::dvec2(2 as f64, 3 as f64),
+        };
+
+        assert!(rectangle.intersects_with_circle(&circle));
+        assert!(circle.intersects_with_rectangle(&rectangle));
+    }
+
+    #[test]
+    fn circle_inside_rectangle() {
+        let rectangle = Rectangle {
+            dims: glm::dvec2(5 as f64, 7 as f64),
+            center: glm::dvec2(4 as f64, 2 as f64),
+        };
+
+        let circle = Circle {
+            radius: 1 as f64,
+            center: glm::dvec2(5 as f64, 3 as f64),
+        };
+
+        assert!(rectangle.intersects_with_circle(&circle));
+        assert!(circle.intersects_with_rectangle(&rectangle));
     }
 }
