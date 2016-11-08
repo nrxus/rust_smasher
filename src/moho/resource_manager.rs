50f092039a9e91c44ff40c1e5572525c2794e875
Andres Medina <amedina@pivotal.io>
2016-11-08 12:31:36 -0800

    use refcell and rc to hide mutability from getTexture

--- src/moho/resource_manager.rs
+++ src/moho/resource_manager.rs
@@ -4,6 +4,8 @@ use sdl2::render::{Renderer, Texture};
 use sdl2_image::LoadTexture;
 use std::path::Path;
 use std::collections::HashMap;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 trait ImageLoader {
     type Image;
@@ -18,35 +20,41 @@ impl<'a> ImageLoader for Renderer<'a> {
 }
 
 struct ResourceManager<'a, I: ImageLoader> {
-    texture_cache: HashMap<&'a str, I::Image>,
+    texture_cache: RefCell<HashMap<&'a str, Rc<I::Image>>>,
     image_loader: I,
 }
 
 impl<'a, I: ImageLoader> ResourceManager<'a, I> {
     pub fn new(image_loader: I) -> Self {
         ResourceManager {
-            texture_cache: HashMap::new(),
+            texture_cache: RefCell::new(HashMap::new()),
             image_loader: image_loader,
         }
     }
-    pub fn getTexture(&mut self, path: &'a str) -> Result<&I::Image, String> {
-        if !self.texture_cache.contains_key(path) {
-            let image_path = Path::new(path);
-            let texture = try!(self.image_loader.load_from_path(image_path));
-            self.texture_cache.insert(path, texture);
+    pub fn getTexture(&self, path: &'a str) -> Result<Rc<I::Image>, String> {
+        {
+            let cache = self.texture_cache.borrow();
+            let image = cache.get(path);
+            if let Some(x) = image {
+                return Ok(x.clone());
+            }
         }
-        self.texture_cache.get(path).ok_or("Texture Cache Error: No such path".into())
+        let mut cache = self.texture_cache.borrow_mut();
+        let image_path = Path::new(path);
+        let image = Rc::new(try!(self.image_loader.load_from_path(image_path)));
+        cache.insert(path, image.clone());
+        Ok(image.clone())
     }
 }
 
 #[cfg(test)]
 mod test {
     use std::path::Path;
-    use sdl2::render::Texture;
-    use std::cell::{RefCell, Ref};
+    use std::cell::RefCell;
     use super::ImageLoader;
     use super::ResourceManager;
 
+    #[derive(Debug)]
     struct MockImage {
         pub path: String,
         pub id: u16,
@@ -76,11 +84,11 @@ mod test {
 
     #[test]
     fn loads_image() {
-        let imageLoader = MockImageLoader {
+        let image_loader = MockImageLoader {
             load_count: RefCell::new(0),
             error: None,
         };
-        let mut subject = ResourceManager::new(imageLoader);
+        let subject = ResourceManager::new(image_loader);
         let image = subject.getTexture("mypath/").unwrap();
         assert_eq!(image.path, String::from("mypath/"));
         assert_eq!(image.id, 1);
@@ -88,41 +96,36 @@ mod test {
 
     #[test]
     fn returns_error() {
-        let imageLoader = MockImageLoader {
+        let image_loader = MockImageLoader {
             load_count: RefCell::new(0),
             error: Some("FAIL".into()),
         };
-        let mut subject = ResourceManager::new(imageLoader);
+        let subject = ResourceManager::new(image_loader);
         let image = subject.getTexture("mypath/");
         assert_eq!(image.err(), Some("FAIL".into()));
     }
 
     #[test]
     fn caches_images() {
-        let imageLoader = MockImageLoader {
+        let image_loader = MockImageLoader {
             load_count: RefCell::new(0),
             error: None,
         };
+        let subject = ResourceManager::new(image_loader);
 
-        let mut subject = ResourceManager::new(imageLoader);
+        // get a new image - ID will be 1
+        let image1 = subject.getTexture("mypath/1").unwrap();
+        assert_eq!(image1.path, String::from("mypath/1"));
+        assert_eq!(image1.id, 1);
 
-        {
-            let image = subject.getTexture("mypath/1").unwrap();
-            assert_eq!(image.path, String::from("mypath/1"));
-            assert_eq!(image.id, 1);
-        }
+        // get the image again - ID should not change
+        let image2 = subject.getTexture("mypath/1").unwrap();
+        assert_eq!(image2.path, String::from("mypath/1"));
+        assert_eq!(image2.id, 1);
 
-
-        // no error because image is already cached
-        {
-            let image = subject.getTexture("mypath/1").unwrap();
-            assert_eq!(image.path, String::from("mypath/1"));
-            assert_eq!(image.id, 1);
-        }
-
-        // no error because image is already cached
-        let image = subject.getTexture("mypath/2").unwrap();
-        assert_eq!(image.path, String::from("mypath/2"));
-        assert_eq!(image.id, 2);
+        // get another time - ID should be a new one
+        let image3 = subject.getTexture("mypath/2").unwrap();
+        assert_eq!(image3.path, String::from("mypath/2"));
+        assert_eq!(image3.id, 2);
     }
 }
