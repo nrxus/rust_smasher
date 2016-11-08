b4bb421bd96421c7335ae42e8f6639102624052c
Andres Medina <amedina@pivotal.io>
2016-11-07 16:41:21 -0800

    split update/draw in animation
    if not repeating reset counter then return false and reset instant to
    nothing when we reach the max frame; otherwise simply restart from 0

--- src/animation.rs
+++ src/animation.rs
@@ -12,16 +12,18 @@ pub struct Animation {
     current_frame: u32,
     frame_duration: Duration,
     frame_instant: Option<Instant>,
+    repeat: bool,
 }
 
 impl Animation {
     pub fn new(texture: Texture,
                num_frames: u32,
                frame_duration_ms: u16,
-               wrapping_coords: Option<glm::UVec2>)
+               wrapping_coords: Option<glm::UVec2>,
+               repeat: bool)
                -> Self {
 
-        let sprite = SpriteStrip::new(texture, 1, wrapping_coords);
+        let sprite = SpriteStrip::new(texture, num_frames, wrapping_coords);
 
         Animation {
             sprite: sprite,
@@ -29,15 +31,40 @@ impl Animation {
             current_frame: 0,
             frame_duration: Duration::from_millis(frame_duration_ms as u64),
             frame_instant: None,
+            repeat: repeat,
         }
     }
 
-    pub fn draw(&mut self, renderer: &mut Renderer, center: glm::IVec2) -> Result<(), String> {
-        let frame_rect = match self.frame_instant {
-            None => {}
-            Some(_) => {}
+    pub fn update(&mut self) -> bool {
+        self.current_frame = match self.frame_instant {
+            None => {
+                self.frame_instant = Some(Instant::now());
+                0
+            }
+            Some(instant) => {
+                if instant.elapsed() >= self.frame_duration {
+                    self.frame_instant = Some(instant + self.frame_duration);
+                    self.current_frame + 1
+                } else {
+                    self.current_frame
+                }
+            }
         };
 
+        if self.current_frame > self.num_frames {
+            if self.repeat {
+                self.current_frame -= self.num_frames;
+                true
+            } else {
+                self.frame_instant = None;
+                false
+            }
+        } else {
+            true
+        }
+    }
+
+    pub fn draw(&self, renderer: &mut Renderer, center: glm::IVec2) -> Result<(), String> {
         self.sprite.draw(renderer, center, self.current_frame)
     }
 }
