a16acc006e57bedca3c658cac09d5e0d4561304c
Andres <andresnrx@gmail.com>
2016-12-28 19:51:56 -0800

    created helper methods to update meteor and launch vector dots

--- src/master_smasher.rs
+++ src/master_smasher.rs
@@ -66,6 +66,24 @@ impl<E: MohoEngine> MasterSmasher<E> {
     }
 
     fn update(&mut self) -> bool {
+        if !self.input_manager.update() || self.input_manager.is_key_down(Keycode::Escape) {
+            return false;
+        }
+
+        if self.meteor.is_launched() {
+            if self.input_manager.did_press_key(Keycode::R) {
+                self.meteor.restart_at(glm::ivec2(50, 50));
+            } else {
+                self.update_meteor();
+            }
+        } else {
+            if self.input_manager.did_click_mouse(MouseButton::Left) {
+                self.meteor.launch(self.input_manager.mouse_coords());
+            } else {
+                self.update_launch_vector();
+            }
+        }
+
         let explosion_ended = match self.explosion {
             Some(ref mut expl) => !expl.update(),
             None => false,
@@ -74,48 +92,6 @@ impl<E: MohoEngine> MasterSmasher<E> {
         if explosion_ended {
             self.explosion = None
         };
-
-        if !self.input_manager.update() || self.input_manager.is_key_down(Keycode::Escape) {
-            return false;
-        }
-
-        if !self.meteor.is_launched() {
-            let mouse_coords = self.input_manager.mouse_coords();
-            let mouse_coords = glm::dvec2(mouse_coords.x as f64, mouse_coords.y as f64);
-            let meteor = self.meteor.collision_body();
-            let distance = mouse_coords - meteor.center;
-            let offset = meteor.radius + 10.;
-            let offset_vector = normalize_to(distance, offset);
-            let anchor_point = meteor.center + offset_vector;
-            let step = (mouse_coords - anchor_point) / (self.rects.len() as f64);
-
-            for (i, rect) in self.rects.iter_mut().enumerate() {
-                let point = anchor_point + (step * i as f64);
-                rect.center_on((point.x as i32, point.y as i32));
-            }
-        }
-
-        if self.input_manager.did_click_mouse(MouseButton::Left) && !self.meteor.is_launched() {
-            self.meteor.launch(self.input_manager.mouse_coords());
-        }
-
-        if self.input_manager.did_press_key(Keycode::R) {
-            self.meteor.restart_at(glm::ivec2(50, 50));
-        }
-
-        self.meteor.update();
-        let meteor_body = self.meteor.collision_body();
-
-        if meteor_body.intersects(&self.planet.collision_body()) {
-            let explosion_path = "resources/explosion_large.png";
-            let explosion_texture = self.renderer.load_texture(explosion_path).unwrap();
-            let explosion_sprite = SpriteStrip::new(explosion_texture, 8, None);
-            let animation = Animation::new(explosion_sprite, 8, false, 80);
-            let center = glm::ivec2(meteor_body.get_center().x as i32,
-                                    meteor_body.get_center().y as i32);
-            self.explosion = Some(Explosion::new(animation, center));
-            self.meteor.restart_at(glm::ivec2(50, 50));
-        }
 
         true
     }
@@ -133,5 +109,37 @@ impl<E: MohoEngine> MasterSmasher<E> {
         }
         self.renderer.present();
         Ok(())
+    }
+
+    fn update_meteor(&mut self) {
+        self.meteor.update();
+
+        let meteor_body = self.meteor.collision_body();
+        if meteor_body.intersects(&self.planet.collision_body()) {
+            let explosion_path = "resources/explosion_large.png";
+            let explosion_texture = self.renderer.load_texture(explosion_path).unwrap();
+            let explosion_sprite = SpriteStrip::new(explosion_texture, 8, None);
+            let animation = Animation::new(explosion_sprite, 8, false, 80);
+            let center = glm::ivec2(meteor_body.get_center().x as i32,
+                                    meteor_body.get_center().y as i32);
+            self.explosion = Some(Explosion::new(animation, center));
+            self.meteor.restart_at(glm::ivec2(50, 50));
+        }
+    }
+
+    fn update_launch_vector(&mut self) {
+        let mouse_coords = self.input_manager.mouse_coords();
+        let mouse_coords = glm::dvec2(mouse_coords.x as f64, mouse_coords.y as f64);
+        let meteor = self.meteor.collision_body();
+        let distance = mouse_coords - meteor.center;
+        let offset = meteor.radius + 10.;
+        let offset_vector = normalize_to(distance, offset);
+        let anchor_point = meteor.center + offset_vector;
+        let step = (mouse_coords - anchor_point) / (self.rects.len() as f64);
+
+        for (i, rect) in self.rects.iter_mut().enumerate() {
+            let point = anchor_point + (step * i as f64);
+            rect.center_on((point.x as i32, point.y as i32));
+        }
     }
 }
