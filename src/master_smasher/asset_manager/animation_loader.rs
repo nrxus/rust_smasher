f6591a88bcc56a5469842f59b721742e3afd6a6e
Andres <andresnrx@gmail.com>
2017-01-31 00:14:39 -0800

    split animation loader from asset manager

---
+++ src/master_smasher/asset_manager/animation_loader.rs
@@ -0,0 +1,63 @@
+use glm;
+use moho::errors::*;
+use moho::frame_animator::FrameAnimator;
+use moho::renderer::Renderer;
+use moho::resource_manager::{ResourceManager, Texture};
+use moho::tile_sheet::TileSheet;
+
+use std::time::Duration;
+
+#[derive(Clone)]
+pub struct AnimationData {
+    pub texture: Texture,
+    pub animator: FrameAnimator,
+    pub sheet: TileSheet,
+}
+
+impl AnimationData {
+    pub fn new<R>(path: &'static str,
+                  frames: u32,
+                  duration_ms: u64,
+                  repeat: bool,
+                  resource_manager: &ResourceManager<R>)
+                  -> Result<AnimationData>
+        where R: Renderer
+    {
+        let texture = resource_manager.load_texture(path)?;
+        let duration = Duration::from_millis(duration_ms);
+        let sheet = TileSheet::new(glm::uvec2(frames, 1));
+        let animator = FrameAnimator::new(frames, duration, repeat);
+        let data = AnimationData {
+            texture: texture,
+            sheet: sheet,
+            animator: animator,
+        };
+        Ok(data)
+    }
+}
+
+pub fn load_star<R: Renderer>(resource_manager: &ResourceManager<R>) -> Result<AnimationData> {
+    static PATH: &'static str = "resources/star.png";
+    const FRAMES: u32 = 2;
+    const DURATION_MS: u64 = 150;
+
+    AnimationData::new(PATH, FRAMES, DURATION_MS, true, resource_manager)
+}
+
+pub fn load_small_explosion<R: Renderer>(resource_manager: &ResourceManager<R>)
+                                         -> Result<AnimationData> {
+    static PATH: &'static str = "resources/explosion_small.png";
+    const FRAMES: u32 = 10;
+    const DURATION_MS: u64 = 100;
+
+    AnimationData::new(PATH, FRAMES, DURATION_MS, false, resource_manager)
+}
+
+pub fn load_large_explosion<R: Renderer>(resource_manager: &ResourceManager<R>)
+                                         -> Result<AnimationData> {
+    static PATH: &'static str = "resources/explosion_large.png";
+    const FRAMES: u32 = 8;
+    const DURATION_MS: u64 = 80;
+
+    AnimationData::new(PATH, FRAMES, DURATION_MS, false, resource_manager)
+}
--- src/master_smasher/asset_manager/mod.rs
+++ src/master_smasher/asset_manager/mod.rs
@@ -1,18 +1,16 @@
 pub mod animation;
 pub mod asset;
+mod animation_loader;
 
 pub use self::animation::Animation;
 pub use self::asset::Asset;
+use self::animation_loader::*;
 
 use glm;
 use moho::errors::*;
-use moho::frame_animator::FrameAnimator;
 use moho::renderer::Renderer;
 use moho::resource_manager::{ResourceManager, Texture};
-use moho::tile_sheet::TileSheet;
 use sdl2::rect;
-
-use std::time::Duration;
 
 pub enum Drawable<'a> {
     Asset(&'a Asset),
@@ -47,13 +45,6 @@ pub enum AnimationAsset {
     Star,
 }
 
-#[derive(Clone)]
-struct AnimationData {
-    texture: Texture,
-    animator: FrameAnimator,
-    sheet: TileSheet,
-}
-
 pub struct AssetManager {
     red_planet: Texture,
     white_planet: Texture,
@@ -72,9 +63,9 @@ impl AssetManager {
     pub fn new<R>(resource_manager: &ResourceManager<R>) -> Result<Self>
         where R: Renderer
     {
-        let star = Self::load_star(resource_manager)?;
-        let explosion_small = Self::load_small_explosion(resource_manager)?;
-        let explosion_large = Self::load_large_explosion(resource_manager)?;
+        let star = load_star(resource_manager)?;
+        let explosion_small = load_small_explosion(resource_manager)?;
+        let explosion_large = load_large_explosion(resource_manager)?;
         let red_planet = resource_manager.load_texture("resources/red_planet.png")?;
         let white_planet = resource_manager.load_texture("resources/white_planet.png")?;
         let blue_planet = resource_manager.load_texture("resources/blue_planet.png")?;
@@ -121,51 +112,5 @@ impl AssetManager {
         let dims = glm::uvec2(dims.x / data.animator.num_frames(), dims.y);
         let asset = Asset::centered_on(data.texture.id, center, dims);
         Animation::new(asset, data.sheet.clone(), data.animator.clone())
-    }
-
-    fn load_star<R: Renderer>(resource_manager: &ResourceManager<R>) -> Result<AnimationData> {
-        static PATH: &'static str = "resources/star.png";
-        const FRAMES: u32 = 2;
-        const DURATION_MS: u64 = 150;
-
-        Self::load_animation(PATH, FRAMES, DURATION_MS, true, resource_manager)
-    }
-
-    fn load_small_explosion<R: Renderer>(resource_manager: &ResourceManager<R>)
-                                         -> Result<AnimationData> {
-        static PATH: &'static str = "resources/explosion_small.png";
-        const FRAMES: u32 = 10;
-        const DURATION_MS: u64 = 100;
-
-        Self::load_animation(PATH, FRAMES, DURATION_MS, false, resource_manager)
-    }
-
-    fn load_large_explosion<R: Renderer>(resource_manager: &ResourceManager<R>)
-                                         -> Result<AnimationData> {
-        static PATH: &'static str = "resources/explosion_large.png";
-        const FRAMES: u32 = 8;
-        const DURATION_MS: u64 = 80;
-
-        Self::load_animation(PATH, FRAMES, DURATION_MS, false, resource_manager)
-    }
-
-    fn load_animation<R>(path: &'static str,
-                         frames: u32,
-                         duration_ms: u64,
-                         repeat: bool,
-                         resource_manager: &ResourceManager<R>)
-                         -> Result<AnimationData>
-        where R: Renderer
-    {
-        let texture = resource_manager.load_texture(path)?;
-        let duration = Duration::from_millis(duration_ms);
-        let sheet = TileSheet::new(glm::uvec2(frames, 1));
-        let animator = FrameAnimator::new(frames, duration, repeat);
-        let data = AnimationData {
-            texture: texture,
-            sheet: sheet,
-            animator: animator,
-        };
-        Ok(data)
     }
 }
