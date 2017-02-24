256e228b384bde2aa4f90347bb48e3d6381b7ff1
Andres <andresnrx@gmail.com>
2017-02-23 22:33:43 -0800

    first pass at fixed-step loop

--- src/master_smasher/mod.rs
+++ src/master_smasher/mod.rs
@@ -6,7 +6,6 @@ use self::level::Level;
 
 use errors::*;
 use moho::input_manager::InputManager;
-use moho::renderer::Renderer;
 use moho::resource_manager::{ResourceManager, Texture};
 use moho::timer::Timer;
 use moho::MohoEngine;
@@ -38,26 +37,31 @@ impl<E: MohoEngine> MasterSmasher<E> {
     }
 
     pub fn run(&mut self) -> Result<()> {
+        const GAME_SPEED: u32 = 60;
+        const MAX_SKIP: u32 = 10;
+        let update_duration = Duration::new(0, 1000000000 / GAME_SPEED);
         let mut timer = Timer::new();
+        let mut delta: Duration = Default::default();
         while !self.game_quit() {
             let game_time = timer.update();
-            self.update();
+            delta += game_time.since_update;
+            let mut loops: u32 = 0;
+            while delta >= update_duration && loops < MAX_SKIP {
+                self.input_manager.update();
+                if self.game_quit() {
+                    break;
+                }
+                self.update();
+                delta -= update_duration;
+                loops += 1;
+            }
             self.draw(game_time.since_update)?;
         }
         Ok(())
     }
 
     fn update(&mut self) {
-        self.input_manager.update();
-        if self.game_quit() {
-            return;
-        }
-
         self.level.update(&self.input_manager);
-    }
-
-    fn game_quit(&self) -> bool {
-        self.input_manager.game_quit() || self.input_manager.is_key_down(Keycode::Escape)
     }
 
     fn draw(&mut self, delta: Duration) -> Result<()> {
@@ -70,5 +74,9 @@ impl<E: MohoEngine> MasterSmasher<E> {
         }
         self.renderer.present();
         Ok(())
+    }
+
+    fn game_quit(&self) -> bool {
+        self.input_manager.game_quit() || self.input_manager.is_key_down(Keycode::Escape)
     }
 }
