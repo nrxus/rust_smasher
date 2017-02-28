c674c9a30eef510938f724848dd4c427c7ba138a
Andres <andresnrx@gmail.com>
2017-02-27 18:03:13 -0800

    do not force state types to implement Copy

--- src/master_smasher/level/interpolate.rs
+++ src/master_smasher/level/interpolate.rs
@@ -1,19 +1,21 @@
 use glm;
 use master_smasher::shape::Circle;
 
+use std::mem;
+
 pub trait Interpolate {
-    fn interpolate(&self, next: Self, interpolation: f64) -> Self;
+    fn interpolate(&self, next: &Self, interpolation: f64) -> Self;
 }
 
 impl Interpolate for glm::IVec2 {
-    fn interpolate(&self, next: glm::IVec2, interpolation: f64) -> glm::IVec2 {
-        let delta = next - *self;
+    fn interpolate(&self, next: &glm::IVec2, interpolation: f64) -> glm::IVec2 {
+        let delta = *next - *self;
         *self + glm::to_ivec2(glm::to_dvec2(delta) * interpolation)
     }
 }
 
 impl Interpolate for Circle {
-    fn interpolate(&self, next: Circle, interpolation: f64) -> Circle {
+    fn interpolate(&self, next: &Circle, interpolation: f64) -> Circle {
         let delta_radius = next.radius - self.radius;
         let delta_center = next.center - self.center;
         Circle {
@@ -28,20 +30,23 @@ pub struct State<T> {
     pub current: T,
 }
 
-impl<T: Copy + Interpolate> State<T> {
+impl<T> State<T> {
+    pub fn update(&mut self, new: T) {
+        self.old = mem::replace(&mut self.current, new);
+    }
+}
+
+impl<T: Clone> State<T> {
     pub fn new(state: T) -> Self {
         State {
-            old: state,
+            old: state.clone(),
             current: state,
         }
     }
+}
 
-    pub fn update(&mut self, new: T) {
-        self.old = self.current;
-        self.current = new;
-    }
-
+impl<T: Interpolate> State<T> {
     pub fn interpolated(&self, interpolation: f64) -> T {
-        self.old.interpolate(self.current, interpolation)
+        self.old.interpolate(&self.current, interpolation)
     }
 }
