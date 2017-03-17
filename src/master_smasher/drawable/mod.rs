9de0d0413b56467e859d426194697a577bd98d5c
Andres <andresnrx@gmail.com>
2017-03-16 21:53:13 -0700

    added drawable trait that is able to draw given a position and renderer

--- src/master_smasher/drawable/mod.rs
+++ src/master_smasher/drawable/mod.rs
@@ -12,6 +12,8 @@ use moho::resource_manager::ResourceManager;
 
 use std::slice::Iter;
 
+use glm;
+
 pub trait TryIterator {
     type Item;
     fn try<F>(self, action: F) -> Result<()> where F: FnMut(&Self::Item) -> Result<()>;
@@ -29,16 +31,22 @@ impl<'a, T> TryIterator for Iter<'a, T> {
     }
 }
 
+pub trait Drawable<R: GameRenderer> {
+    fn draw(&self, position: glm::IVec2, renderer: &mut R) -> Result<()>;
+}
+
 pub trait Scene<R: GameRenderer> {
     fn show(&self, renderer: &mut R) -> Result<()>;
 }
 
 pub trait GameRenderer: Sized {
-    fn show<D: Scene<Self>>(&mut self, drawable: &D) -> Result<()>;
-}
+    fn show<S: Scene<Self>>(&mut self, scene: &S) -> Result<()> {
+        scene.show(self)
+    }
 
-impl<R: Renderer> GameRenderer for ResourceManager<R> {
-    fn show<D: Scene<ResourceManager<R>>>(&mut self, drawable: &D) -> Result<()> {
-        drawable.show(self)
+    fn draw<D: Drawable<Self>>(&mut self, drawable: &D, position: glm::IVec2) -> Result<()> {
+        drawable.draw(position, self)
     }
 }
+
+impl<R: Renderer> GameRenderer for ResourceManager<R> {}
